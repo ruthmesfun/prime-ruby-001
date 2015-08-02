c1502ad414f5cdf539ee5452b4b7d01add4ce9a1
ruth.mesfun <ruth.mesfun@flatironschool.com>
2015-08-02 08:59:24 -0400

    All Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(num)
+  divisors = [2, 3,5,7,9,11]
+  dividend = num
+  composite = Array.new
+
+  divisors.each do |div|
+    if dividend == div
+      true
+    elsif dividend % div == 0
+      composite <<  dividend
+    end
+    composite
+  end
+
+  if composite.empty? 
+    true
+  else 
+    false
+  end
+end
